83f4a54271a67487d45790745d3f00f8d225ac17
VVK1978 <cdroom@mail.ru>
2021-04-09 18:37:51 +0300

    feat: add 5&6 task

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -1,3 +1,4 @@
+/* eslint-disable radix */
 /**
  * Given some integer, find the maximal number you can obtain
  * by deleting exactly one digit of the given number.
@@ -9,8 +10,17 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit (n) {
+  let arr = Array.from(n.toString());
+  const res = [];
+  if (arr.length === 1) return n;
+  if (arr.length === 2) return parseInt(arr.sort((a, b) => b - a)[0]);
+  arr.forEach((el, ind) => {
+    arr.splice(ind, 1);
+    res.push(parseInt(arr.join('')));
+    arr = Array.from(n.toString());
+  });
+  return res.sort((a, b) => b - a)[0];
 }
 
 module.exports = deleteDigit;
