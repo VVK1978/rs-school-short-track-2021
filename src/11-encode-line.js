528978d567b2e7dedfa2f20c2ac15adc6ae61bd3
VVK1978 <70840645+VVK1978@users.noreply.github.com>
2021-04-11 19:11:25 +0300

    feat: add task 11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -9,7 +9,22 @@
  *
  */
 function encodeLine(str) {
-  throw new Error('Not implemented');
+  const array = str.split('');
+  let res = '';
+  let num = 1;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] !== array[i + 1]) {
+      if (num === 1) {
+        res += array[i];
+      } else {
+        res += num.toString() + array[i];
+        num = 1;
+      }
+    } else {
+      num += 1;
+    }
+  }
+  return res;
 }
 
 module.exports = encodeLine;
